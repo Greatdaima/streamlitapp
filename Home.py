849087ef68918556aa08da4101560a00b74310c6
Greatdaima <2080459994@qq.com>
2023-10-01 11:23:41 +0800

    再次提交

--- Home.py
+++ Home.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.graph_objs as go
+
+
+
 # 设置页面配置
 st.set_page_config(page_title="我的论文", page_icon=":chart_with_upwards_trend:",
                    layout="centered",  # centered  wide
