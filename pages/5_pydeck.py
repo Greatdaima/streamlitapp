bb07b370a1dadc14abdf494baa341eb45f6a95e5
Greatdaima <2080459994@qq.com>
2023-10-02 19:59:26 +0800

    origin

--- pages/5_pydeck.py
+++ pages/5_pydeck.py
@@ -4,15 +4,15 @@ import numpy as np
 import pydeck as pdk
 
 chart_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [50, 50] + [34.81732, 113.535807],
     columns=['lat', 'lon'])
 
 st.pydeck_chart(pdk.Deck(
-    map_style=None,
+    map_style='mapbox://styles/mapbox/satellite-streets-v11',
     initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
+        latitude=34.81732,
+        longitude=113.535807,
+        zoom=10,
         pitch=50,
     ),
     layers=[
@@ -20,7 +20,7 @@ st.pydeck_chart(pdk.Deck(
             'HexagonLayer',
             data=chart_data,
             get_position='[lon, lat]',
-            radius=200,
+            radius=80,
             elevation_scale=4,
             elevation_range=[0, 1000],
             pickable=True,
@@ -31,7 +31,7 @@ st.pydeck_chart(pdk.Deck(
             data=chart_data,
             get_position='[lon, lat]',
             get_color='[200, 30, 0, 160]',
-            get_radius=200,
+            get_radius=20,
         ),
     ],
 ))
