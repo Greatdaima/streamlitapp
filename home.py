cf08143b19a8d1bcf839766303805ff68769e949
Greatdaima <2080459994@qq.com>
2023-10-05 16:15:05 +0800

    提交

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="个人展示仓库", page_icon=":chart_with_upwar
 if 'pwd' not in st.session_state:
     st.session_state['pwd'] = ''
 
-# woden
+# 怎么上传啊
 with st.container():
     st.write('展示3D图')
 
